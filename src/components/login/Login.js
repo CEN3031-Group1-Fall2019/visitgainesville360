cd2f5942df3cb740e9ce99112d52175102c5bb4d
Alana Erickson <alanaerickson@ufl.edu>
2019-11-04 13:22:24 -0500

    Implements basic login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,57 @@
 import React from 'react';
 
 class Login extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			email: '',
+			password: '',
+		};
+	}
+
+	handleChange(state) {
+		this.setState({ 
+			[state.target.id]: state.target.value 
+		});
+	};
+
 	render() {
 		return (
-			<div><p>LOGIN PAGE</p></div>
+			<div className="container">
+				<p className="h2 p-3">Login</p>
+				<form>
+				<div className="row align-items-center">
+					<div className="col-4">Email</div>
+					<div className="col-12">
+						<input
+							onChange={this.handleChange}
+							value={this.state.email}
+							id="email"
+							type="email"
+						/>
+					</div>
+				</div>
+				<div className="row align-items-center">
+					<div className="col-4">Password</div>
+					<div className="col-12">
+						<input
+							onChange={this.handleChange}
+							value={this.state.password}
+							id="password"
+							type="password"
+						/>
+					</div>
+				</div>
+
+				<div className="row-12 p-3">
+					<button
+						type="submit"
+						className="btn btn-success">
+						Login
+					</button>
+				</div>
+				</form>
+			</div>
 		);
 	}
 }
