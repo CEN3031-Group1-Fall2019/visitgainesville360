745858d598392b8645e9259e9e92437be860bcf8
TK Farewell <mfarewell@ufl.edu>
2019-11-10 03:49:58 -0500

    Edited app.js according to heroku tutorial

--- src/server/config/app.js
+++ src/server/config/app.js
@@ -5,6 +5,13 @@ var config = require('./config'),
 	passport = require("passport"),
 	users = require('../routes/user.router');
 
+
+
+// Heroku tutorial addition:
+const path = require('path');
+
+
+
 module.exports.start = function() {
 	const app = express();
 
@@ -20,6 +27,25 @@ module.exports.start = function() {
   
   	app.use(passport.initialize());
   	require('./passport')(passport);
+  	
+
+
+
+
+  	// Added for Heroku:
+	// Server static assets if in production
+	if(process.env.NODE_ENV === 'production') {
+		// set static folder
+		app.use(express.static('client/build'));
+
+		app.get('*', (req, res) = > {
+			res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	 	});
+	}
+  	
+  	
+  	
+  	
 
 	// Configures router
 	app.use("/users", users);
