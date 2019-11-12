ce027fc88491aa7349d105df29e499e4db4f91e1
Alana Erickson <alanaerickson@ufl.edu>
2019-11-12 14:41:58 -0500

    Adds check path

--- src/server/config/app.js
+++ src/server/config/app.js
@@ -28,7 +28,9 @@ module.exports.start = function() {
 
 	/** Heroku **/
 	var path = require("path");
-	app.use(express.static(path.resolve(__dirname + '/../../client')));
+	app.use(express.static(path.resolve(__dirname + './../../client')));
+	var checkPath = (__dirname + './../../client');
+	console.log("Pathing to: ", checkPath);
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname + '/../../../public/index.html'));
