9c95ea07ac10f1b0faaa9d58c9654412aacf42a3
Alana Erickson <alanaerickson@ufl.edu>
2019-12-10 15:45:30 -0500

    add heroku to app.js

--- src/server/config/app.js
+++ src/server/config/app.js
@@ -29,6 +29,14 @@ module.exports.start = function() {
 	app.use("/users", users);
 	app.use("/listings", listings);
 	app.use("/admin", admin);
+
+	/** Heroku **/
+	var path = require("path");
+	app.use(express.static(path.join(__dirname + './../../../build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname + '/../../../public/index.html'));
+	});
 	  
 	var port = process.env.PORT || 5000;
   	app.listen(port, function() {
