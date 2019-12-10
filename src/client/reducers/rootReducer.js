488c8d3ad7a7ba3beaaac2e5f8fb22e441f675ad
Alana Erickson <alanaerickson@ufl.edu>
2019-12-10 15:29:45 -0500

    Removes error for bootstrap

--- src/client/reducers/rootReducer.js
+++ src/client/reducers/rootReducer.js
@@ -1,8 +1,6 @@
 import {combineReducers} from "redux";
 import loginReducer from "./loginReducer";
-import listingReducer from "./listingReducer";
 
 export default combineReducers ({
-	login: loginReducer,
-	listing: listingReducer
+	login: loginReducer
 });
