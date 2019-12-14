695ad98127bce683ceb9a80241b079666c0ef371
Alana Erickson <alanaerickson@ufl.edu>
2019-12-13 19:52:01 -0500

    Changes app get

--- src/server/config/app.js
+++ src/server/config/app.js
@@ -31,7 +31,7 @@ module.exports.start = function() {
 	var path = require("path");
 	app.use(express.static(path.join(__dirname + './../../../build')));
 
-	app.get('*', (req, res) => {
+	app.get('/', (req, res) => {
 		res.sendFile(path.resolve(__dirname + '/../../../public/index.html'));
 	});
 	  
